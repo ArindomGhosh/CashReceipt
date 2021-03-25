2b2c0b90b298d7b336c0476a58bbc1939a58ec65
arindom ghosh <arindomghosh23@gmail.com>
2021-03-25 19:47:56 +0530

    unit test for receipt details API

--- app/src/test/java/com/arindom/cashrecipt/network/CashReceiptApiTest.kt
+++ app/src/test/java/com/arindom/cashrecipt/network/CashReceiptApiTest.kt
@@ -3,8 +3,10 @@ package com.arindom.cashrecipt.network
 import com.arindom.cashrecipt.exception.UserNotFoundException
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
 import org.hamcrest.CoreMatchers
 import org.junit.Assert.assertThat
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class CashReceiptApiTest {
@@ -12,23 +14,37 @@ class CashReceiptApiTest {
     @Test
     fun `should return UserNotFoundException provided random id`() {
         val cashReceiptService = FakeCashReceiptServiceImpl()
-        val resultStates =cashReceiptService.getCashReceiptDetails(5)
-        runBlocking {
-            resultStates.collect{
+        val resultStates = cashReceiptService.getCashReceiptDetails(5)
+        runBlockingTest {
+            resultStates.collect {
                 assertThat(it, CoreMatchers.instanceOf(ResultStates.Failure::class.java))
                 val failureState = it as ResultStates.Failure
-                assertThat(failureState.error, CoreMatchers.instanceOf(UserNotFoundException::class.java))
+                assertThat(failureState.error,
+                    CoreMatchers.instanceOf(UserNotFoundException::class.java))
             }
         }
     }
 
     @Test
-    fun `should return receipt detail provided correct id`(){
+    fun `should return receipt detail provided correct id`() {
         val cashReceiptService = FakeCashReceiptServiceImpl()
-        val resultStates =cashReceiptService.getCashReceiptDetails(85)
-        runBlocking {
-            resultStates.collect{
+        val resultStates = cashReceiptService.getCashReceiptDetails(85)
+        runBlockingTest {
+            resultStates.collect {
                 assertThat(it, CoreMatchers.instanceOf(ResultStates.Success::class.java))
+            }
+        }
+    }
+
+    @Test
+    fun `should return receipt details screen layout `() {
+        val cashReceiptService = FakeCashReceiptServiceImpl()
+        val resultStates =cashReceiptService.getReceiptDetailsLayout()
+        runBlockingTest {
+            resultStates.collect {
+                assertThat(it, CoreMatchers.instanceOf(ResultStates.Success::class.java))
+                val successState = it as ResultStates.Success
+                assertTrue(successState.data.rows.isNotEmpty())
             }
         }
     }
